511afaa9e03cc552962efbd54c8a25183954dd58
Jem Altieri <jem@jem.space>
2022-11-15 00:20:39 -0500

    reactification

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import logo from './logo512.png';
 import './App.css';
 import React, { useState } from 'react';
 
+const FETCH_URL = 'https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint8'
+
 const MEGAMILLIONS = [
   {
       name: "White Balls",
@@ -15,57 +17,12 @@ const MEGAMILLIONS = [
       min: 1,
       max: 25
   },
-]
-
-function QuantumNumberProvider() {
-  this.bag = []
-
-  var refillBag = () => {
-    var onFetchResult = (result) => {
-      //console.log("got result");
-      this.bag = result.data;
-    }
-  
-    var onFetchError = (e) => {
-      //console.log("fetch error")
-      //console.log(e)
-    }
-  
-    //console.log("fetching from remote source");
-    fetch('https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint8')
-    .then(res => res.json())
-    .then(onFetchResult, onFetchError);
-  }
-
-  this.draw = (min, max) => {
-    if (this.bag.length == 0) {
-      refillBag();
-    }
-    let n = this.bag.pop();
-    while (n < min || n > max) {
-      if (this.bag.length == 0) {
-        refillBag();
-      }
-      n = this.bag.pop();
-    }
-    return n
-  }
-}
+];
 
 function GameDisplay({pickedGame}) {
-  // let headerCols = this.props.pickedgame.map((pool, i) => {
-  //   if (pool.numPicks > 1) {
-  //     return <colgroup span={pool.numPicks} key={i}></colgroup>
-  //   } else {
-  //     return <col key={i}></col>
-  //   }
-  // })
   if (pickedGame == null) {
-    //console.log("is null")
     return null
   }
-  //console.log("pickedGame:")
-  //console.log(pickedGame);
   let headerCells = pickedGame.map((pool, i) => {
     if (!pool) {
       return null
@@ -74,7 +31,6 @@ function GameDisplay({pickedGame}) {
       return (
         <th
           colSpan={`${pool.numPicks}`}
-          // scope="colgroup"
           key={i}
         >{pool.name}</th>
       )
@@ -95,7 +51,6 @@ function GameDisplay({pickedGame}) {
   });
   return (
     <table>
-      {/* {headerCols} */}
       <thead>
         <tr>
           {headerCells}
@@ -130,67 +85,52 @@ function Oracle() {
   const [pickedGame, updatePickedGame] = useState([]);
 
   function parsePicks(result) {
-    //console.log("in parsepicks");
-    //console.log(result);
     let data = result.data;
     let d_i = 0;
     return MEGAMILLIONS.map(pool => {
-      //console.log("in map")
-      //console.log("pool is:");
-      //console.log(pool);
       let picks = [];
       for (let i = 0; i < pool.numPicks; i++) {
-        //console.log("picking a number with i "+i);
         while (picks.includes(data[d_i]) || data[d_i] > pool.max || data[d_i] < pool.min) { //FIXME: guard against running out of data
           d_i++;
         }
         picks.push(data[d_i]);
-        //console.log("picks: "+picks);
         d_i++;
       }
       return {
         ...pool,
         picks: picks,
       }
-    })
+    });
   }
 
   var onFetchResult = (result) => {
-    //console.log("got result");
     updatePickedGame(parsePicks(result));
     updateRequestCompleted(true);
   }
 
   var onFetchError = (e) => {
-    //console.log("fetch error")
-    //console.log(e)
+    console.log("fetch error")
+    console.log(e)
     updateRequestCompleted(true);
   }
 
   var fetchExternalNumbers = () => {
-    //console.log("fetching from remote source");
     updateRequestCompleted(false);
     updateRequestStarted(true);
-    fetch('https://qrng.anu.edu.au/API/jsonI.php?length=1024&type=uint8')
+    fetch(FETCH_URL)
     .then(res => res.json())
     .then(onFetchResult, onFetchError);
   }
 
   var handleConsult = (e) => {
     e.preventDefault();
-    //console.log('You clicked consult.');
     fetchExternalNumbers();
   }
 
-  // const { error } = this.state;
-  // if (this.state.error) {
-  //   //console.log("error in Oracle state");
-  // }
-
   let pickDisplay = null;
-  if (requestCompleted == true) {
+  if (requestCompleted === true) {
     pickDisplay = (
-      <GameDisplay pickedGame={pickedGame}/>
+      <GameDisplay pickedGame={pickedGame} />
     )
   }
   return (
